e35ba51605ec27d1557197163e14cd75f71cbe31
zt-logics <zahaib.tariq123@gmail.com>
2025-07-24 20:31:30 +0000

    life in js

---
+++ 03_basics/03_life.js
@@ -0,0 +1,21 @@
+// Immediately Invoked Function Expression (IIFE)
+
+
+(function code() {
+    // Named IIFE
+    console.log("DB Connect");
+    
+}) ();
+                    // if two IIFE's write togather we use " ; " in the above 
+
+
+((name) => {
+    // Unamed IIFE
+    console.log(`My Name is ${name}`);
+    
+}) ("Zahaib Tariq")
+
+
+
+
+
