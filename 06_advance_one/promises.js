9ce316ce45c53d74b8afc641dda57c09e21aebf6
zt-logics <zahaib.tariq123@gmail.com>
2025-09-03 20:58:57 +0000

    done

--- 06_advance_one/promises.js
+++ 06_advance_one/promises.js
@@ -39,3 +39,83 @@ promiseThree.then(function(user){
     console.log(user);
     
 })
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "Zahaib Tariq", password: "1118"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    })
+})
+
+promiseFour.then((user) => {
+    console.log(user)
+    return user.username;
+}).then((myUsername) => {
+    console.log(myUsername);
+    
+}).catch(function(error) {
+    console.log(error);
+}).finally(() => console.log('The promise is either resolved or rejected'));
+
+
+const promiseFive = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "JS", password: "1118"})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    })
+})
+
+async function consumePromiseFrive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
+consumePromiseFrive()
+
+
+
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json();
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log("E: ", error);
+        
+//     }
+// }
+
+// getAllUsers()
+
+
+
+
+fetch('https://api.github.com/users/zt-logics')
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    
+})
+.catch((error) => {
+    console.log(error);
+    
+})
